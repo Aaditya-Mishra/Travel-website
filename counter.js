6dcd4fd61139ea24fc3213e7e7d11f5ea92411fc
Aaditya Mishra <aadityamishra216@gmail.com>
2022-05-06 21:14:44 +0530

    first commit

---
+++ counter.js
@@ -0,0 +1,9 @@
+var i = 0;
+
+function incrementValue() {
+    document.getElementById('number').value = ++i;
+}
+function decrementValue() {
+ 
+        document.getElementById('number').value=--i;
+}
